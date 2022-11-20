a83539efd4d11871c5a36ccfe74d69a9da630878
joshuameyers01 <jmeyer29@students.kennesaw.edu>
2022-11-20 15:42:24 -0500

    fixed typo col_in to col in and added color definitions

--- Game of Life.py
+++ Game of Life.py
@@ -2,7 +2,7 @@ import time
 import pygame
 import numpy as np
 
-COLOR-BG = (10, 10, 10)
+COLOR_BG = (10, 10, 10)
 COLOR_GRID = (40, 40, 40)
 COLOR_DIE_NEXT = (170, 170, 170)
 COLOR_ALIVE_NEXT = (255, 255, 255)
@@ -11,6 +11,15 @@ COLOR_ALIVE_NEXT = (255, 255, 255)
 def update(screen, cells, size, with_progress=False):
     updated_cells= np.empty((cells.shape[0], cells.shape[1]))
 
-    for row, col_in np.ndindex(cells.shape):
+    for row, col in np.ndindex(cells.shape):
         alive= np.sum(cells[row-1:row+2, col-1:col+2]) - cells[row, col]
-        color= 
+        color= COLOR_BG if cells[row, col] == 0 else COLOR_ALIVE_NEXT
+
+        if cells[row, col] == 1:
+            if alive < 2 or alive > 3:
+                if with_progress:
+                    color = COLOR_DIE_NEXT
+            elif 2 <= alive <= 3:
+                updated_cells[row, col] = 1
+                if with_progress:
+                    color = COLOR_ALIVE_NEXT
